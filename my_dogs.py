25629b5cbcd52b67de90c3bfe653b99664fb0ee0
MackRoe <elaine.music@students.makeschool.com>
2019-11-19 14:40:26 -0800

    first commit, again

---
+++ my_dogs.py
@@ -0,0 +1,8 @@
+# my_dogs.py
+import dog
+
+# instantiation call that creates a Dog object
+my_dog = dog.Dog("Rex", "SuperDog")
+my_other_dog = dog.Dog("Annie", "SuperDog")
+
+print(my_other_dog.name)
