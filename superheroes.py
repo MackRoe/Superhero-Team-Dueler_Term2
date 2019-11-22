df3b506bf28879a3e75a96a4917e86fed90b70ae
MackRoe <elaine.music@students.makeschool.com>
2019-11-21 20:52:46 -0800

    add Ability and Armor classes

--- superheroes.py
+++ superheroes.py
@@ -11,7 +11,8 @@ class Ability:
         # Assign the "name" and "max_damage"
         # for a specific instance of the Ability class
         self.name = name
-        self.max_damage = max_damage
+        self.max_damage = 100
+        self.attack_strength = attack_strength
 
     def attack(self):
         ''' Return a value between 0 and the value set by self.max_damage.'''
@@ -19,3 +20,11 @@ class Ability:
         # Pick a random value between 0 and self.max_damage
         random_value = random.randint(0, self.max_damage)
         return random_value
+
+
+if __name__ == "__main__":
+    # If you run this file from the terminal
+    # this block is executed.
+    ability = Ability("Debugging Ability", 20)
+    print(ability.name)
+    print(ability.attack())
