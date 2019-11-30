1ddd8361b77446350c5665f91ad4a6ef79fa18eb
MackRoe <elaine.music@students.makeschool.com>
2019-11-30 11:21:32 -0800

    move Ability class into its own file

--- superheroes.py
+++ superheroes.py
@@ -1,27 +1,8 @@
-import random
 from armor import Armor
 from hero import Hero
+from ability import Ability
 
 
-class Ability:
-    def __init__(self, name, attack_strength):
-        '''
-       Initialize the values passed into this
-       method as instance variables.
-        '''
-
-        # Assign the "name" and "max_damage"
-        # for a specific instance of the Ability class
-        self.name = name
-        self.max_damage = 100
-        self.attack_strength = attack_strength
-
-    def attack(self):
-        ''' Return a value between 0 and the value set by self.max_damage.'''
-
-        # Pick a random value between 0 and self.max_damage
-        random_value = random.randint(0, self.max_damage)
-        return random_value
 
 
 if __name__ == "__main__":
