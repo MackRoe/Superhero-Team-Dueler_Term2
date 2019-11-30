13052e0a88acfb4d96925f40e79ef03eacfaafc4
MackRoe <elaine.music@students.makeschool.com>
2019-11-30 10:46:54 -0800

    add Armor class

--- armor.py
+++ armor.py
@@ -7,3 +7,11 @@ class Armor:
         # TODO: Create instance variables for the values passed in.
         self.name = name
         self.max_block = max_block
+
+    def block(self):
+        '''
+        Return a random value between 0 and the
+        initialized max_block strength.
+        '''
+        blocked = randint(0: self.max_block)
+        return blocked
