f0107c843661e9eebfe39ed411648fe0c4e2a54f
MackRoe <elaine.music@students.makeschool.com>
2019-12-02 17:57:00 -0800

    create_ability method added to Arena class

--- arena.py
+++ arena.py
@@ -6,3 +6,13 @@ class Arena:
         '''
         self.team_one = team_one
         self.team_two = team_two
+
+    def create_ability(self):
+        '''Prompt for Ability information.
+            return Ability with values from user Input
+        '''
+        name = input("What is the ability name?  ")
+        max_damage = input(
+            "What is the max damage of the ability?  ")
+
+        return Ability(name, max_damage)
