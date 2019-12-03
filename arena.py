b0f309faf7f5e5c77e71bda3f364ed2fa73f8a7f
MackRoe <elaine.music@students.makeschool.com>
2019-12-02 17:55:03 -0800

    create arena class

---
+++ arena.py
@@ -0,0 +1,8 @@
+class Arena:
+    def __init__(self):
+        '''Instantiate properties
+            team_one: None
+            team_two: None
+        '''
+        self.team_one = team_one
+        self.team_two = team_two
