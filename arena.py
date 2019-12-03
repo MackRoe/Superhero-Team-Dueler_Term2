9e47b7a0f219e6f04ebf9576cf7e899a34c47486
MackRoe <elaine.music@students.makeschool.com>
2019-12-02 18:35:52 -0800

    create_weapon & create_armor methods added to Arena class

--- arena.py
+++ arena.py
@@ -16,3 +16,48 @@ class Arena:
             "What is the max damage of the ability?  ")
 
         return Ability(name, max_damage)
+
+    def create_weapon(self):
+        '''Prompt user for Weapon information
+            return Weapon with values from user input.
+        '''
+        name = input("What is the weapon?  ")
+        max_damage = input(
+            "What is the max damage of the weapon?  ")
+
+        return Weapon(name, max_damage)
+
+    def create_armor(self):
+        '''Prompt user for Armor information
+          return Armor with values from user input.
+        '''
+        # TODO:This method will allow a user to create a piece of armor.
+        #  Prompt the user for the necessary information to create a new
+        #   armor object.
+        #  return the new armor object with values set by user.
+        name = input("What is the name of the armor?  ")
+        max_damage = input(
+            "What is the max damage of the armor?  ")
+
+        return Armor(name, max_damage)
+
+    def create_hero(self):
+        '''Prompt user for Hero information
+          return Hero with values from user input.
+        '''
+        hero_name = input("Hero's name: ")
+        hero = Hero(hero_name)
+        add_item = None
+        while add_item != "4":
+            print("[1] Add ability\n[2] Add weapon\n[3] Add armor")
+            add_item = input("[4] Done adding items\n\nYour choice: ")
+            if add_item == "1":
+                # add an ability to the hero
+                create_ability()
+            elif add_item == "2":
+                # add a weapon to the hero
+                create_weapon()
+            elif add_item == "3":
+                # add an armor to the hero
+                create_armor()
+        return hero
